ca5b81466b1e151f48677d80f49d26dc621e8226
JulianBenitez <julian.benitezg99@gmail.com>
2022-11-14 22:53:25 -0500

    Update redirect_url amplify

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,6 @@
 const config = {
     "cognito_hosted_domain": "aygo-04.auth.us-east-1.amazoncognito.com",
-    "redirect_url": "http://localhost:3000/",
+    "redirect_url": "https://master.dhpvyvj8nw1l0.amplifyapp.com/",
     "cognito_client_id": "1c6i41d8bq8004gk9mt2si61qs",
     "user_pool_id": "us-east-1_ekZUPiWAz",
     "wss_url": "wss://6h50n49w98.execute-api.us-east-1.amazonaws.com/test",
